b28c7befd979e9209a16a8237db1a85e4f325050
Bibek Lama <netshering@gmail.com@users.noreply.github.com>
2020-09-04 14:23:21 +0200

    Update POM and Launcher

--- neo4j-micro-service/src/main/java/fr/epita/neo4j/launcher/SpringBootLauncher.java
+++ neo4j-micro-service/src/main/java/fr/epita/neo4j/launcher/SpringBootLauncher.java
@@ -2,10 +2,17 @@ package fr.epita.neo4j.launcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages="fr.epita")
-public class SpringBootLauncher {
+public class SpringBootLauncher extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootLauncher.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLauncher.class, args);
 	}
