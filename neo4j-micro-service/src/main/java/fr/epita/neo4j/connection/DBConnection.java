909932fc13a287a41ad1eb058d730e0f1e8af1c5
Bibek Lama <netshering@gmail.com@users.noreply.github.com>
2020-09-09 15:50:40 +0200

    Update POM

--- neo4j-micro-service/src/main/java/fr/epita/neo4j/connection/DBConnection.java
+++ neo4j-micro-service/src/main/java/fr/epita/neo4j/connection/DBConnection.java
@@ -5,7 +5,7 @@ import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 
 public class DBConnection {
-	final String neo4jURL = "bolt://192.168.0.15:10687";
+	final String neo4jURL = "bolt://192.168.0.34:10687";
 	final String neo4jUser = "neo4j";
 	final String neo4jPass = "epita";
 	Driver driver;
